c3a40294f64a0b8ea5a2085f46e72f90e2f3d6c9
maithachanh <anh.mt06051@sinhvien.hoasen.edu.vn>
2024-12-03 22:27:32 +0700

    Update ProgrammerCalculatorUI.dart

--- lib/ProgrammerCalc/ProgrammerCalculatorUI.dart
+++ lib/ProgrammerCalc/ProgrammerCalculatorUI.dart
@@ -1,4 +1,3 @@
-// ProgrammerCalculatorUI.dart
 import 'package:flutter/material.dart';
 import 'ProgrammerCalculatorHandler.dart';
 
@@ -9,8 +8,11 @@ class ProgrammerCalculatorUI extends StatefulWidget {
 
 class _ProgrammerCalculatorUIState extends State<ProgrammerCalculatorUI> {
   String _input = '';
-  String _result = '';
-  String _base = 'Decimal';
+  String _resultDecimal = '0';
+  String _resultBinary = '0';
+  String _resultHexadecimal = '0';
+  String _resultOctal = '0';
+  String _base = 'Hexadecimal';
 
   void _appendToInput(String value) {
     setState(() {
@@ -22,13 +24,21 @@ class _ProgrammerCalculatorUIState extends State<ProgrammerCalculatorUI> {
     try {
       String evaluationResult =
           ProgrammerCalculatorHandler.evaluate(_input, _base);
+
+      List<String> results = evaluationResult.split('\n');
       setState(() {
-        _result = evaluationResult;
-        _input = ''; // Clear input after calculation
+        _resultDecimal = results[0].split(': ')[1];
+        _resultBinary = results[1].split(': ')[1];
+        _resultHexadecimal = results[2].split(': ')[1];
+        _resultOctal = results[3].split(': ')[1];
+        _input = '';
       });
     } catch (e) {
       setState(() {
-        _result = 'Invalid Input';
+        _resultDecimal = 'Invalid Input';
+        _resultBinary = 'Invalid Input';
+        _resultHexadecimal = 'Invalid Input';
+        _resultOctal = 'Invalid Input';
       });
     }
   }
@@ -36,149 +46,122 @@ class _ProgrammerCalculatorUIState extends State<ProgrammerCalculatorUI> {
   void _clear() {
     setState(() {
       _input = '';
-      _result = '';
+      _resultDecimal = '0';
+      _resultBinary = '0';
+      _resultHexadecimal = '0';
+      _resultOctal = '0';
     });
   }
 
   void _setBase(String base) {
     setState(() {
       _base = base;
-      _input = ''; // Clear input when changing base
-      _result = '';
+      _input = '';
+      _clear();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Container(
-            alignment: Alignment.centerRight,
-            child: Text(
-              'Input ($_base): $_input',
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            alignment: Alignment.centerRight,
-            child: Text(
-              'Result:\n$_result',
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () => _setBase('Binary'),
-                child: Text('Binary'),
-              ),
-              ElevatedButton(
-                onPressed: () => _setBase('Decimal'),
-                child: Text('Decimal'),
-              ),
-              ElevatedButton(
-                onPressed: () => _setBase('Hexadecimal'),
-                child: Text('Hexadecimal'),
-              ),
-              ElevatedButton(
-                onPressed: () => _setBase('Octal'),
-                child: Text('Octal'),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Expanded(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _buildButton('7'),
-                    _buildButton('8'),
-                    _buildButton('9'),
-                    _buildButton('AND'),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _buildButton('4'),
-                    _buildButton('5'),
-                    _buildButton('6'),
-                    _buildButton('OR'),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _buildButton('1'),
-                    _buildButton('2'),
-                    _buildButton('3'),
-                    _buildButton('XOR'),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _buildButton('0'),
-                    _buildButton('C'),
-                    _buildButton('='),
-                    _buildButton('NOT'),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Programmer Calculator'),
+        backgroundColor: Colors.teal,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            _buildDisplaySection(),
+            const SizedBox(height: 50),
+            _buildBaseSwitcher(),
+            const SizedBox(height: 40),
+            _buildButtonsGrid(),
+          ],
+        ),
       ),
     );
   }
 
+  Widget _buildDisplaySection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Text(
+          'Input ($_base): $_input',
+          style: const TextStyle(fontSize: 25),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          'Decimal: $_resultDecimal',
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'Binary: $_resultBinary',
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'Hexadecimal: $_resultHexadecimal',
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'Octal: $_resultOctal',
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBaseSwitcher() {
+    final bases = ['Binary', 'Decimal', 'Hexadecimal', 'Octal'];
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: bases
+          .map((base) => ElevatedButton(
+                onPressed: () => _setBase(base),
+                child: Text(base),
+              ))
+          .toList(),
+    );
+  }
+
+  Widget _buildButtonsGrid() {
+    final buttonLabels = [
+      ['A', '%', '(', ')', 'AC'],
+      ['B', 'AND', 'OR', 'XOR', 'NOT'],
+      ['C', '7', '8', '9', '/'],
+      ['D', '4', '5', '6', 'X'],
+      ['E', '1', '2', '3', '-'],
+      ['F', '.', '0', '=', '+']
+    ];
+    return Column(
+      children: buttonLabels
+          .map((row) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: row.map((label) => _buildButton(label)).toList(),
+              ))
+          .toList(),
+    );
+  }
+
   Widget _buildButton(String value) {
-    return Container(
-      margin: EdgeInsets.all(4.0),
+    return SizedBox(
+      width: 100,
+      height: 50,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.teal,
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
+            borderRadius: BorderRadius.circular(20.0),
           ),
-          fixedSize: Size(70, 70), // Fixed size for buttons
         ),
         onPressed: () {
           if (value == '=') {
             _calculateResult();
-          } else if (value == 'C') {
-            _clear();
-          } else if (value == 'AND' || value == 'OR' || value == 'XOR') {
-            // Handle bitwise operations
-            int a = int.parse(_input);
-            int b = int.parse(_input); // For simplicity, using the same input
-            int result;
-            if (value == 'AND') {
-              result = ProgrammerCalculatorHandler.performBitwiseAnd(a, b);
-            } else if (value == 'OR') {
-              result = ProgrammerCalculatorHandler.performBitwiseOr(a, b);
-            } else {
-              result = ProgrammerCalculatorHandler.performBitwiseXor(a, b);
-            }
+          } else if (value == 'AC') {
             setState(() {
-              _result = 'Result: $result';
-              _input = ''; // Clear input after calculation
-            });
-          } else if (value == 'NOT') {
-            int a = int.parse(_input);
-            int result = ProgrammerCalculatorHandler.performBitwiseNot(a);
-            setState(() {
-              _result = 'Result: $result';
-              _input = ''; // Clear input after calculation
+              _input = '';
             });
           } else {
             _appendToInput(value);
@@ -186,7 +169,7 @@ class _ProgrammerCalculatorUIState extends State<ProgrammerCalculatorUI> {
         },
         child: Text(
           value,
-          style: TextStyle(fontSize: 24, color: Colors.white),
+          style: const TextStyle(fontSize: 16, color: Colors.white),
         ),
       ),
     );
