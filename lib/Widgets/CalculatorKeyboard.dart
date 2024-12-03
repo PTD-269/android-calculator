0f56ffaa0354bf9b9d9abe9bc841351d5caae7e9
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-12-03 20:13:42 +0700

    raw

--- lib/Widgets/CalculatorKeyboard.dart
+++ lib/Widgets/CalculatorKeyboard.dart
@@ -1,48 +1,91 @@
 import 'package:flutter/material.dart';
 
 class CalculatorKeyboard extends StatelessWidget {
-  final Function(String) onButtonPressed; // Add the callback function as a parameter
+  final Function(String) onButtonPressed;
 
   // Constructor to receive the callback function
-  CalculatorKeyboard({Key? key, required this.onButtonPressed}) : super(key: key);
+  CalculatorKeyboard({Key? key, required this.onButtonPressed})
+      : super(key: key);
 
   final List<String> buttons = [
-    "C", "DEL", "%", "/",
-    "9", "8", "7", "x",
-    "6", "5", "4", "-",
-    "3", "2", "1", "+",
-    "0", ".", "ANS", "=",
+    "More",
+    "C",
+    "%",
+    "/",
+    "9",
+    "8",
+    "7",
+    "x",
+    "6",
+    "5",
+    "4",
+    "-",
+    "3",
+    "2",
+    "1",
+    "+",
+    "0",
+    ".",
+    "=",
   ];
+
+  Color getButtonColor(String button) {
+   // map with same key as color 
+    final Map<String, Color> buttonColors = {
+      "More": Colors.white38,
+      "C": Colors.white70,
+      "=": Colors.green,
+      "/": Colors.orange,
+      "x": Colors.orange,
+      "-": Colors.orange,
+      "+": Colors.orange,
+    };
+
+    
+    return buttonColors[button] ?? Colors.blue;
+  }
 
   @override
   Widget build(BuildContext context) {
+    // size of screen
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    //size of buttons
+    double buttonWidth = (screenWidth - 40) / 4; 
+    double buttonHeight = buttonWidth * 0.75; // Tăng tỷ lệ chiều cao của nút (tuỳ chỉnh)
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: GridView.builder(
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: buttons.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4, // 4 columns in GridView
-          childAspectRatio: 1.5, // Ratio of button size, makes buttons wider
-          crossAxisSpacing: 8,  // Space between columns
-          mainAxisSpacing: 8,   // Space between rows
-        ),
-        itemBuilder: (context, index) {
-          return ElevatedButton(
-            onPressed: () => onButtonPressed(buttons[index]), // Trigger the callback here
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue, // Background color of the button
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8), // Rounded corners for the buttons
+      child: Expanded (
+        child: GridView.builder(
+          // physics: const NeverScrollableScrollPhysics(),
+          itemCount: buttons.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            childAspectRatio: buttonWidth / buttonHeight,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+          ),
+          itemBuilder: (context, index) {
+            String button = buttons[index];
+            Color buttonColor = getButtonColor(button);
+        
+            return ElevatedButton(
+              onPressed: () => onButtonPressed(button),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: buttonColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: EdgeInsets.symmetric(vertical: buttonWidth * 0.2),
               ),
-              padding: EdgeInsets.symmetric(vertical: 20), // Button height
-            ),
-            child: Text(
-              buttons[index],
-              style: TextStyle(fontSize: 24, color: Colors.white), // Button text style
-            ),
-          );
-        },
+              child: Text(
+                button,
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
