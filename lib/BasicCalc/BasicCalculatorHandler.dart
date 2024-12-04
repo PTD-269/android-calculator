332e876bd96b05d64535c0cfe8064887c4a02d6e
Pham Dat <122384368+PTD-269@users.noreply.github.com>
2024-12-04 19:39:48 +0700

    Update BasicCalculatorHandler.dart

--- lib/BasicCalc/BasicCalculatorHandler.dart
+++ lib/BasicCalc/BasicCalculatorHandler.dart
@@ -1,52 +1,7 @@
+import 'dart:math';
+
 class BasicCalculatorHandler {
-  static double evaluate(String expression) {
-    expression = _removeSpaces(expression);
-    List<String> tokens = _splitTokens(expression);
-    List<String> operators = _extractOperators(expression);
-
-    tokens = _performMultiplicationAndDivision(tokens, operators);
-    return _performAdditionAndSubtraction(tokens, operators);
-  }
-
-  static String _removeSpaces(String expression) {
-    return expression.replaceAll(' ', '');
-  }
-
-  static List<String> _splitTokens(String expression) {
-    return expression.split(RegExp(r'(\+|\-|\*|\/)'));
-  }
-
-  static List<String> _extractOperators(String expression) {
-    return expression.split(RegExp(r'[0-9\.]+'))..removeWhere((e) => e.isEmpty);
-  }
-
-  static List<String> _performMultiplicationAndDivision(
-      List<String> tokens, List<String> operators) {
-    for (int i = 0; i < operators.length; i++) {
-      if (operators[i] == '*' || operators[i] == '/') {
-        double a = double.parse(tokens[i]);
-        double b = double.parse(tokens[i + 1]);
-        double result = operators[i] == '*' ? a * b : a / b;
-        tokens[i] = result.toString();
-        tokens.removeAt(i + 1);
-        operators.removeAt(i);
-        i--;
-      }
-    }
-    return tokens;
-  }
-
-  static double _performAdditionAndSubtraction(
-      List<String> tokens, List<String> operators) {
-    double result = double.parse(tokens[0]);
-    for (int i = 0; i < operators.length; i++) {
-      double b = double.parse(tokens[i + 1]);
-      if (operators[i] == '+') {
-        result += b;
-      } else if (operators[i] == '-') {
-        result -= b;
-      }
-    }
-    return result;
-  }
+  // Static method to evaluate a mathematical expression
+  static double? evaluateExpression(String expression) {
+    return null;
 }
