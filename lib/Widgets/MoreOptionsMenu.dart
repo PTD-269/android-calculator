95773844c2143700f75fcaa20c43e4dd78c73b69
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-12-11 17:15:44 +0700

    fix fe 1

--- lib/Widgets/MoreOptionsMenu.dart
+++ lib/Widgets/MoreOptionsMenu.dart
@@ -1,58 +1,3 @@
-// import 'package:flutter/material.dart';
-// class MoreOptionsMenu extends StatelessWidget {
-//   final Function(String) onOptionSelected;
-
-//   MoreOptionsMenu({required this.onOptionSelected});
-
-//   final List<String> moreOptions = [
-//     "Sin","Cos","Tan","Cosin"
-//   ]; 
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return PopupMenuButton<String>(
-//       onSelected: onOptionSelected,
-//       itemBuilder: (BuildContext context) {
-//         return moreOptions.map((String option) {
-//           return PopupMenuItem<String>(
-//             value: option,
-//             child: Container(
-//               width: 150,
-//               padding: EdgeInsets.all(10),
-//               decoration: BoxDecoration(
-//                 color: Colors.blueAccent, // Chỉnh màu nền
-//                 borderRadius: BorderRadius.circular(8),
-//                 boxShadow: [
-//                   BoxShadow(
-//                     color: Colors.black26,
-//                     blurRadius: 5,
-//                     offset: Offset(0, 2),
-//                   ),
-//                 ],
-//               ),
-//               child: Center(
-//                 child: Text(
-//                   option,
-                  
-//                   style: TextStyle(
-                    
-
-//                     fontSize: 18,
-//                     fontWeight: FontWeight.bold,
-//                     color: Colors.white,
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           );
-//         }).toList();
-//       },
-//     );
-//   }
-// }
-//ver 2
-
-
 
 import 'package:flutter/material.dart';
 
@@ -67,13 +12,13 @@ class MoreOptionsMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Lấy kích thước màn hình từ MediaQuery
+    // media query to get size 
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
-    // Tính toán padding và chiều rộng hộp dựa trên kích thước màn hình
-    double horizontalPadding = screenWidth * 0.05; // 5% chiều rộng màn hình
-    double verticalPadding = screenHeight * 0.03;  // 3% chiều cao màn hình
+
+    double horizontalPadding = screenWidth * 0.05; 
+    double verticalPadding = screenHeight * 0.03;  
 
     return PopupMenuButton<String>(
       onSelected: onOptionSelected,
@@ -82,20 +27,21 @@ class MoreOptionsMenu extends StatelessWidget {
           return PopupMenuItem<String>(
             value: option,
             child: Container(
-              // Chiều rộng hộp tự động điều chỉnh theo màn hình
-              width: screenWidth * 0.4, // Chiếm 40% chiều rộng màn hình
+             // height adjust depending on device
+              width: screenWidth * 0.4, 
               padding: EdgeInsets.symmetric(
-                horizontal: horizontalPadding, // Padding ngang tự tính
-                vertical: verticalPadding,    // Padding dọc tự tính
+                horizontal: horizontalPadding, 
+                vertical: verticalPadding,   
               ),
+              // box 
               decoration: BoxDecoration(
-                color: Colors.blueAccent, // Màu nền của hộp
-                borderRadius: BorderRadius.circular(8), // Bo tròn góc
+                color: Colors.blueAccent, 
+                borderRadius: BorderRadius.circular(8), 
                 boxShadow: [
                   BoxShadow(
                     color: Colors.black26,
                     blurRadius: 5,
-                    offset: Offset(0, 2), // Hiệu ứng bóng
+                    offset: Offset(0, 2), 
                   ),
                 ],
               ),
